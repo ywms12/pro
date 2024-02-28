1c563b61b492e3cb7d49081f41cba053c2de8a3c
yymsa <msallamyazan619@gmail.com>
2024-02-28 11:26:58 +0300

    pppp

---
+++ main.js
@@ -0,0 +1,17 @@
+document.getElementById("showEle").addEventListener("click",function(){
+    document.getElementById("showEle").style.display = "none";
+    document.getElementById("element").style.display = "block";
+});
+document.getElementById("hideEle").addEventListener("click",function(){
+    document.getElementById("showEle").style.display = "block";
+    document.getElementById("element").style.display = "none";
+});
+/* THE SECOND */
+document.getElementById("showElee").addEventListener("click",function(){
+    document.getElementById("ele2").style.display = "none";
+    document.getElementById("ele").style.display = "block";
+});
+document.getElementById("showEle2").addEventListener("click",function(){
+    document.getElementById("ele").style.display = "none";
+    document.getElementById("ele2").style.display = "block";
+});
